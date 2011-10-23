3e4e1d2a899aa6e6f568c04a98a730974b2afe88
Paul Nikitochkin <pftg@jetthoughts.com>
2011-10-24 01:52:50 +0300

    added unicron start

--- cookbooks/vagrant-main/recipes/default.rb
+++ cookbooks/vagrant-main/recipes/default.rb
@@ -1,1 +1,20 @@
 require_recipe "main"
+
+gem_package 'bundler'
+
+#execute "bundle gems" do
+#  #user "vagrant"
+#  cwd "/vagrant"
+#  command "bundle install"
+#  #returns 1
+#  action :run
+#end
+
+execute "run unicorn" do
+  user "vagrant"
+  cwd "/vagrant"
+  command "bundle install && bundle exec unicorn . -D -p 8080"
+  not_if
+  returns 1
+  action :run
+end
