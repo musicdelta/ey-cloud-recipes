175dc2d24902f66ba4f0028182ed6419aef2d846
Paul Nikitochkin <pftg@jetthoughts.com>
2011-11-18 16:43:11 +0200

    change remote ip for vpn server

--- cookbooks/openvpn/attributes/default.rb
+++ cookbooks/openvpn/attributes/default.rb
@@ -1,4 +1,4 @@
-openvpn :local => "0.0.0.0", :remote => "vpn.musicdelta-service.com", :port => "5555", :proto => "udp",
+openvpn :local => "0.0.0.0", :remote => "service.musicdelta-demo.com", :port => "5555", :proto => "udp",
         :subnet => "10.10.0.0", :netmask => "255.255.255.0", :log => "/var/log/openvpn.log",
         :key_dir => "/etc/openvpn/server/keys", :signing_ca_key => "/etc/openvpn/server/keys/ca.key",
         :signing_ca_cert => "/etc/openvpn/server/keys/ca.crt",
