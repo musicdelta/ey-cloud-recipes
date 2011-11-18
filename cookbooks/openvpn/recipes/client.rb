ad9241cdfadad9a401e7d564928fd4689108ab47
Paul Nikitochkin <pftg@jetthoughts.com>
2011-11-18 16:30:14 +0200

    fixed openvpn client for chef 0.6.0

--- cookbooks/openvpn/recipes/client.rb
+++ cookbooks/openvpn/recipes/client.rb
@@ -27,16 +27,20 @@ file "#{key_dir}/index.txt" do
   action :create
 end
 
-file "#{key_dir}/serial" do
-  content "01"
+execute "setup #{key_dir}/serial" do
+  command "echo '01' > #{key_dir}/serial"
   not_if { ::File.exists?("#{key_dir}/serial") }
 end
 
 ['ca.crt', "dh#{node["openvpn"]["key"]["size"]}.pem", "ta.key", "client.crt", "client.csr", "client.key"].each do |file|
-  cookbook_file "#{key_dir}/server/#{file}" do
+  remote_file "#{key_dir}/server/#{file}" do
     source file
     mode 0700
   end
+end
+
+service "openvpn.server-client" do
+  action [:enable, :start]
 end
 
 template "/etc/openvpn/server-client.conf" do
@@ -44,9 +48,6 @@ template "/etc/openvpn/server-client.conf" do
   owner "root"
   group "root"
   mode 0644
-  notifies :restart, "service[openvpn.server-client]"
+  notifies :restart, resources(:service => "openvpn.server-client"), :immediate
 end
 
-service "openvpn.server-client" do
-  action [:enable, :start]
-end
